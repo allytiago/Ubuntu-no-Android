29e6156a92cd82c0c060e276b488375365db538f
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-18 18:49:57 -0300

    Update gnome-config.sh

--- config/gnome/gnome-config.sh
+++ config/gnome/gnome-config.sh
@@ -4,7 +4,8 @@ extralink="https://raw.githubusercontent.com/allytiago/Ubuntu-no-Android/main/co
 
 #Get the necessary components
 sudo apt-get update
-sudo apt-get install gnome-shell gnome-terminal gnome-tweaks gnome-shell-extension-ubuntu-dock --no-install-recommends -y
+sudo apt-get install gnome-shell gnome-terminal gnome-tweaks gnome-shell-extension-ubuntu-dock gnome-shell-extensions --no-install-recommends -y
+sudo apt-get install yaru-theme-gnome-shell yaru-theme-icon yaru-theme-gtk 
 
 mkdir -p ~/.vnc
 
@@ -19,3 +20,7 @@ echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
 
 vncpasswd
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+#echo "export PULSE_SERVER=127.0.0.1" >> ~/.bashrc ; bash
