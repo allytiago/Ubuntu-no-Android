e2bee0f953a35013a17e66ef9c6da2555479434d
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-18 22:40:43 -0300

    Update locale.sh

--- config/pt_br/tigervnc/gnome/locale.sh
+++ config/pt_br/tigervnc/gnome/locale.sh
@@ -23,26 +23,28 @@ wget --tries=20 "$extralink/pt_br/tigervnc/vnc" -P /usr/local/bin > /dev/null
 wget --tries=20 "$extralink/pt_br/tigervnc/vncpasswd" -P /usr/local/bin > /dev/null
 wget --tries=20 "$extralink/pt_br/tigervnc/stopvnc" -P /usr/local/bin > /dev/null
 wget --tries=20 "$extralink/pt_br/tigervnc/gnome/startvnc" -P /usr/local/bin > /dev/null
+wget --tries=20 "$extralink/pt_br/tigervnc/gnome/starvncserver" -P /usr/local/bin > /dev/null
 
 
 chmod +x /usr/local/bin/vnc
 chmod +x /usr/local/bin/vncpasswd
 chmod +x /usr/local/bin/startvnc
+chmod +x /usr/local/bin/startvncserver
 chmod +x /usr/local/bin/stopvnc
 
 
-mkdir -p ~/.vnc
+#mkdir -p ~/.vnc
 
-echo '#!/bin/bash
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-export PULSE_SERVER=127.0.0.1
-LANG=pt_BR.UTF-8
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-export XDG_CURRENT_DESKTOP="GNOME"
-service dbus start
-gnome-shell --x11' > ~/.vnc/xstartup
+#echo '#!/bin/bash
+#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+#export PULSE_SERVER=127.0.0.1
+#LANG=pt_BR.UTF-8
+#export LANG
+#[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+#echo $$ > /tmp/xsession.pid
+#export XDG_CURRENT_DESKTOP="GNOME"
+#service dbus start
+#gnome-shell --x11' > ~/.vnc/xstartup
 
-chmod +x ~/.vnc/xstartup
+#chmod +x ~/.vnc/xstartup
