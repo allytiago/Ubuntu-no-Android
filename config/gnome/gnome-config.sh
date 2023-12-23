4a8152d36be19bb8c629817345f07c6ef462211a
AllyTiago <47113005+allytiago@users.noreply.github.com>
2023-12-23 17:43:03 -0300

    Update gnome-config.sh

--- config/gnome/gnome-config.sh
+++ config/gnome/gnome-config.sh
@@ -10,9 +10,18 @@ sudo apt-get install gnome-shell gnome-terminal gnome-tweaks gnome-shell-extensi
 mkdir -p ~/.vnc
 
 echo '#!/bin/bash
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+export PULSE_SERVER=127.0.0.1
+#LANG=pt_BR.UTF-8
+#export LANG
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+echo $$ > /tmp/xsession.pid
+#dbus-launch --exit-with-session startlxde
 export XDG_CURRENT_DESKTOP="GNOME"
 service dbus start
-gnome-shell --x11' > ~/.vnc/xstartup
+gnome-shell --x11
+' > ~/.vnc/xstartup
 
 chmod +x ~/.vnc/xstartup
 
